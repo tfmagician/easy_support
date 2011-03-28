120fbc6df40ef5a2843919d9f41aefad95722614
tfmagician <tfmagician@gmail.com>
2011-03-28 22:00:59 +0900

    Added config/migraionts files.

---
+++ config/migrations/000_initialize.php
@@ -0,0 +1,72 @@
+<?php
+class M4d90866916e843739dce7299fc1400a3 extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ * @access public
+ */
+	public $description = '';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ * @access public
+ */
+	public $migration = array(
+		'up' => array(
+			'create_table' => array(
+				'supports' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+					'mail' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+					'title' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+					'type' => array('type' => 'string', 'null' => false, 'default' => NULL, 'key' => 'index', 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+					'content' => array('type' => 'text', 'null' => false, 'default' => NULL, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+					'sent' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
+					'sent_date' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+					'deleted' => array('type' => 'boolean', 'null' => false, 'default' => '0', 'key' => 'index'),
+					'deleted_date' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+					'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL, 'key' => 'index'),
+					'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+						'type' => array('column' => 'type', 'unique' => 0),
+						'created' => array('column' => 'created', 'unique' => 0),
+						'deleted' => array('column' => 'deleted', 'unique' => 0),
+					),
+					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_unicode_ci', 'engine' => 'InnoDB'),
+				),
+			),
+		),
+		'down' => array(
+			'drop_table' => array(
+				'supports'
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function after($direction) {
+		return true;
+	}
+}
+?>
---
+++ config/migrations/map.php
@@ -0,0 +1,6 @@
+<?php
+$map = array(
+	1 => array(
+		'000_initialize' => 'M4d90866916e843739dce7299fc1400a3'),
+);
+?>
